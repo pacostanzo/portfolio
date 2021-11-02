aa1d22a9a5e43361ec9aaccc969200acd98abd95
Pablo Costanzo <costanzopa@gmail.com>
2021-11-02 00:42:09 -0300

    Adding Resume section

---
+++ public/data/skill.ts
@@ -0,0 +1,7 @@
+import { Skill } from "../../types/Skill"
+
+export const languages: Skill[] = [
+];
+
+export const tools: Skill[] = [
+];
---
+++ types/Skill.ts
@@ -0,0 +1,7 @@
+import { IconType } from "react-icons";
+
+export interface Skill {
+    Icon: IconType;
+    name: string;
+    level: string;
+}
