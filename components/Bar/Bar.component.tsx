06345f4a1fd0112c64e0c768de66bdd32769765e
Pablo Costanzo <costanzopa@gmail.com>
2021-11-03 18:37:29 -0300

    Adding framer motion to bars

--- components/Bar/Bar.component.tsx
+++ components/Bar/Bar.component.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'react';
+import { motion } from 'framer-motion';
 
 import { Skill } from '../../types/Skill';
 
@@ -6,16 +7,34 @@ const Bar: FunctionComponent<{ value: Skill }> = ({
   value: { Icon, level, name },
 }) => {
   const bar_width = `${level}%`;
+  const variants = {
+    initial: {
+      width: 0,
+    },
+    animate: {
+      width: bar_width,
+      transition: {
+        duration: 0.4,
+        type: 'spring',
+        damping: 10, // value of the bounce
+        stiffness: 100, //
+      },
+    },
+  };
+
   return (
-    <div className="my-2 text-white bg-gray-300 rounded-full dark:bg-dark-300 dark:bg-black-500">
-      <div
-        className="flex items-center px-4 py-1 rounded-full bg-gradient-to-r from-green to-blue-500"
+    <div className="my-2 text-white bg-gray-300 rounded-full dark:bg-black-300">
+      <motion.div
+        className="flex items-center px-4 py-1 rounded-full bg-gradient-to-r from-green-800 to-blue-700"
         style={{
           width: bar_width,
         }}
+        variants={variants}
+        initial="initial"
+        animate="animate"
       >
         <Icon className="mr-3" /> {name}
-      </div>
+      </motion.div>
     </div>
   );
 };
