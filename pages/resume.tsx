c0a76f5a55b2dcb10f5655be06b05c666d816502
Pablo Costanzo <costanzopa@gmail.com>
2021-11-03 01:08:56 -0300

    Adding Skills at resume

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,30 +1,37 @@
 import { Bar } from '../components';
-import { languages, tools } from '../public/data/skill';
+import { languages, others } from '../public/data/skill';
 
 const Resume = () => {
   return (
     <div className="px-6 py-2">
+      <h5 className="my-3 text-2xl font-bold">Experience</h5>
       {/* //! Education & Experience */}
       <div className="grid gap-6 md:grid-cols-2">
         <div>
-          <h5 className="my-3 text-2xl font-bold">Education</h5>
           <div className="">
-            <h5 className="my-2 text-xl font-bold">
-              Computer Science Engineering
-            </h5>
-            <p className="font-semibold">Academy of Technology (2017-2021)</p>
+            <h5 className="my-2 text-xl font-bold">Analyst developer</h5>
+            <p className="font-semibold">SouthEnd S.A.</p>
             <p className="my-3">
-              I am currently pursuing B.tech in Computer Science Engineering
-              from Academy of Technology
+              Customization and development of Ariba modules for different
+              clients by using the Java Programming language. Architecture
+              design and development of the supplier management platform
+              (Aquanima Network), based on Vaadin, Grails and Talend.
             </p>
           </div>
         </div>
         <div>
-          <h5 className="my-3 text-2xl font-bold">Experience</h5>
           <div className="">
-            <h5 className="my-2 text-xl font-bold">Software Developer Jr.</h5>
-            <p className="font-semibold">Tata Consultancy Services</p>
-            <p className="my-3">I don't know why I am doing this job</p>
+            <h5 className="my-2 text-xl font-bold">
+              Senior FullStack Developer
+            </h5>
+            <p className="font-semibold">Southend Corp S.A.</p>
+            <p className="my-3">
+              Research, planning and development of various scalable solutions
+              that provide value to SAP Ariba / FieldGlass clients, through the
+              use of Rest and SOAP APIs, intervening in the frontend, backend
+              and infrastructure (React, Spring Boot with Docker and Cloud
+              Foundry).
+            </p>
           </div>
         </div>
       </div>
@@ -41,9 +48,11 @@ const Resume = () => {
         </div>
 
         <div>
-          <h5 className="my-3 text-2xl font-bold">Tools & Softwares</h5>
+          <h5 className="my-3 text-2xl font-bold">
+            Libraries & Infrasctructure
+          </h5>
           <div className="my-2">
-            {tools.map((tool, i) => (
+            {others.map((tool, i) => (
               <Bar value={tool} key={i} />
             ))}
           </div>
